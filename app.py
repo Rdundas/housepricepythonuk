1d8a7166fcf49725b2c4486d9611dd0a86eccd9d
unknown <richdundas@yahoo.com>
2022-08-05 13:35:44 +0100

    Added new changes

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
-#from housepricepythonuk.data.data_load import print_message
-#from housepricepythonuk.data.data_load import load_data
+import pandas as pd
+from dash import dcc, html, Dash, Input, Output
+import plotly.express as px
 
-from datetime import datetime
 
-from data.data_load import print_message
-from data.data_load import load_initial_data
+from config import * 
 
-postcodes=['M20','M21','M33','SK1','SK12','SK15','SK2','SK3'
-,'SK4','SK5','SK6','SK7','SK8','SK9','WA13','WA14','WA15','WA16']
+df=pd.read_csv(current_file)
 
-data_in_filepath="C:\\Users\\richa\\Documents\\DataScience\\House Price\\pp-complete.csv"
-data_out_filepath="C:\\Users\\richa\\Documents\\DataScience\\House Price"
-min_year=2019
+app=Dash(__name__)
 
-df=load_initial_data(data_in_filepath,min_year,postcodes)
-df.to_csv(data_out_filepath+'\\processed'+datetime.now().strftime(('%Y-%m-%d'))+'.csv')
+#get dropdowns
+house_type_dropdown=list(set(df['type']))
+
+#basic layout
+app.layout = html.Div(children=[
+    html.H1(children='Manchester House Price Dashboard'),
+    dcc.Dropdown(id='house_type_dropdown'
+        ,options=[
+        {'label':'S','value':'S'},
+        {'label':'D','value':'D'},
+        {'label':'T','value':'T'},
+        {'label':'O','value':'O'},
+        {'label':'F','value':'F'},
+        ],
+        value='D'
+        ),
+    dcc.Graph(
+        id='house_price_uk_graph'
+    ) 
+])
+
+def create_figure(df):
+    fig=px.line(
+    data_frame=df[['postcode_area','value','year']].groupby(['postcode_area','year']).mean().reset_index()
+    ,x="year",y="value",color='postcode_area'
+    )
+    return fig
+
+@app.callback(Output('house_price_uk_graph','figure'),
+            [Input('house_type_dropdown','value')])
+def update_figure(selected_value):
+    df_out=df[df['type']==selected_value]
+    fig=create_figure(df_out)
+    return fig
+
+    
+if __name__ =='__main__':
+    app.run_server(debug=True)
